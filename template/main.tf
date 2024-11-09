320b2a752cc4bac41cc2087f353738bc41907c06
Jeroen Luchtenberg <j.luchtenberg@protonmail.com>
2024-11-09 18:39:07 +0100

    Remove image and add terraform lintong to workflow

--- template/main.tf
+++ template/main.tf
@@ -1,10 +1,13 @@
 terraform {
+  required_version = "1.9"
   required_providers {
     coder = {
       source = "coder/coder"
+      version = "1.0.4"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
+      version = "2.33.0"
     }
   }
 }
